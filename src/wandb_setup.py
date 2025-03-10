c4f3083ac2a043ca632e43f564f780ce2812f744
Nikshay <nikshay.p.jain@gmail.com>
2025-03-10 13:02:27 +0530

    Updated train, funcs

--- src/wandb_setup.py
+++ src/wandb_setup.py
@@ -1,5 +1,5 @@
 import wandb
 
-def setup_wandb(project_name="DL-Assign-1", run_name=None):
-    wandb.init(project=project_name, name=run_name)
+def setup_wandb(project_name="DL-Assign-1", run_name=None, args=None):
+    wandb.init(project=project_name, name=run_name, config=vars(args) if args else None)
     return wandb
